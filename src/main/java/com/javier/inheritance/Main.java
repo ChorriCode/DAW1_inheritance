9b42d6b6ea9d8e6767c696ffc96b0f651c25ecd7
ChorriCode <compartelo007@yahoo.es>
2021-03-28 17:11:50 +0100

    create objetc with parameter's constructor

--- src/main/java/com/javier/inheritance/Main.java
+++ src/main/java/com/javier/inheritance/Main.java
@@ -5,16 +5,24 @@ public class Main {
 
     public static void main(String[] args) {
         
-        // let's create two objects: from parent Vehicle and from child Car
-        Vehicle vehicle = new Vehicle();
-        Car car = new Car();
-        Bicycle bicycle = new Bicycle();
+        // let's create three objects: from parent Vehicle and from childs Car and Bicycle
+        //Vehicle vehicle = new Vehicle();
+        //Car car = new Car();
+        //Bicycle bicycle = new Bicycle();
+        
+        //let's create objects Vehicle, Car and Bicycle using parameter's contructor
+        //Vehicle vehicle = new Vehicle("SEAT", 4);
+        //Car car = new Car(1500, "BMW", 6);
+        //Bicycle bicycle = new Bicycle("cross");
+        
         //Assign car to vehicle and backwards
         //vehicle = car;
         //car = vehicle;
+        
         //Assign bicycle to vehicle and backwards
         //vehicle = bicycle;
         //bicycle = vehicle;
+        
         //let's create a vehicle of type Car
         //Vehicle vehicle = new Car();
         //vehicle = car;
